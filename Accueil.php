9642064a6e26e1eb1984bf3197ba0e7ee98c18b5
Debzou <david.petit36@gmail.com>
2019-05-10 11:11:39 +0200

    first commit php

---
+++ Accueil.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Acceuil</title>
+</head>
+
+
+<body>
+<center><form  method="post" action="login.php">
+  Votre login : <input type="text" name="login">
+  <br />
+  Votre mtdp : <input type="password" name="pwd" ><br />
+<input type="submit" name="Connexion">
+</form></center>
+
+</body>
+</html>
