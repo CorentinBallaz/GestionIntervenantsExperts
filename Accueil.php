e9cf639b1afb846484a0f3e122819f39ad468e17
Debzou <david.petit36@gmail.com>
2019-05-10 12:01:03 +0200

    bootstrap

--- Accueil.php
+++ Accueil.php
@@ -1,16 +1,29 @@
-<html>
+
+</html>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-<title>Acceuil</title>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
-
-
 <body>
-<center><form  method="post" action="login.php">
-  Votre login : <input type="text" name="login">
+
+<div class="container" >
+  <center><h1 class="alert alert-success">Accueil</h1>
+  <p>Compte de Gesty</p></center>
+</div>
+<center><form class="form-group" method="post" action="login.php">
+  Votre login : <input type="text" class="form-control" name="login">
   <br />
-  Votre mtdp : <input type="password" name="pwd" ><br />
-<input type="submit" name="Connexion">
+  Votre mtdp : <input type="password" class="form-control" name="pwd" ><br />
+<input type="submit" name="Connexion" class="btn btn-dark">
 </form></center>
+
+
 
 </body>
 </html>
