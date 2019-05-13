fe129951dd8644ce5570325cecbf2403efe6c731
Debzou <david.petit36@gmail.com>
2019-05-13 22:45:23 +0200

    Creation de compte

--- Accueil.php
+++ Accueil.php
@@ -1,29 +1,44 @@
 
-</html>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <title>Accueil</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-</head>
-<body>
 
-<div class="container" >
-  <center><h1 class="alert alert-success">Accueil</h1>
-  <p>Compte de Gesty</p></center>
+<div class="row">
+  <div class="col-sm-4"></div>
+  <div class="col-sm-4" style="background-color:#C4E8D9;">
+    </html>
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+      <title>Accueil</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+
+      <h1 class="alert alert-success">Accueil</h1>
+      <p>Compte de Gesty</p>
+
+    <center><form class="form-group" method="post" action="login.php">
+      Votre login : <input  type="text" class="form-control" name="login" required>
+      <br />
+      Votre mtdp : <input type="password" class="form-control" name="pwd" required><br />
+    <input type="submit" name="Connexion" class="btn btn-dark">
+    </form>
+    <a class="p-3 mb-2 bg-dark text-white" href="Creation_compte.php">Pas de compte ?</a>
+    </center>
+    </body>
+    </html>
+  </div>
+  <div class="col-sm-4"></div>
 </div>
-<center><form class="form-group" method="post" action="login.php">
-  Votre login : <input type="text" class="form-control" name="login">
-  <br />
-  Votre mtdp : <input type="password" class="form-control" name="pwd" ><br />
-<input type="submit" name="Connexion" class="btn btn-dark">
-</form></center>
 
+<?php
 
-
-</body>
-</html>
+if (isset($_POST['Creation']))
+{
+  echo $_POST['type'];
+  echo $_POST['login'];
+  echo $_POST['pwd'];
+}
+?>
