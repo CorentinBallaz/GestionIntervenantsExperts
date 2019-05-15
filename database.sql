bae1d03a8ef1b1321b718ce990a29cf53f781bd0
CorentinBallaz <corentin.ballaz@yahoo.fr>
2019-05-15 15:04:45 +0200

    add in domaine_expertise only for the moment

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-create table personne(id_personne integer auto_increment primary key,
+create table if not exists personne(id_personne integer auto_increment primary key,
 		  nom varchar(30),
 		  prenom varchar(30),
 		  login varchar(30),
@@ -6,7 +6,7 @@ create table personne(id_personne integer auto_increment primary key,
 		  type varchar(30)) 
 	ENGINE=INNODB;
 
-create table demande(id_demande integer auto_increment primary key,
+create table if not exists demande(id_demande integer auto_increment primary key,
 		  cours_concerne varchar(30),
 		  domaine_expertise varchar(30),
 		  description varchar(200),
@@ -19,19 +19,21 @@ create table demande(id_demande integer auto_increment primary key,
 		  id_expert integer DEFAULT null)
 	ENGINE=INNODB;
 
-create table cours(id_cours integer auto_increment primary key,
+create table if not exists cours(id_cours integer auto_increment primary key,
 		  nom_cours varchar(30))
 	ENGINE=INNODB;
 
-create table domaine_expertise(id_domaine integer auto_increment primary key,
+create table if not exists domaine_expertise(id_domaine integer auto_increment primary key,
 		  nom_expertise varchar(30))
 	ENGINE=INNODB;
 
-create table aCours(id_cours integer,
+create table if not exists aCours(id_aCours integer auto_increment primary key,
+		  id_cours integer,
 		  id_personne integer)
 	ENGINE=INNODB;
 
-create table estExpert(id_domaine integer,
+create table if not exists estExpert(id_estExpert integer auto_increment primary key,
+		  id_domaine integer,
 		  id_expert integer)
 	ENGINE=INNODB;
 
@@ -48,11 +50,20 @@ ALTER TABLE estExpert
 ADD foreign KEY (id_domaine) references domaine_expertise(id_domaine),
 ADD	foreign KEY (id_expert) references personne(id_personne);
 
-ALTER TABLE aCours
-ADD primary KEY (id_cours,id_personne);
 
-ALTER TABLE estExpert
-ADD primary KEY (id_domaine,id_expert);
+
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Gestion GitHub');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('BlockChain');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Php, CSS, Html');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Java et interfaces graphiques');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Python et utilisation API');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Statistiques et mathématiques générales');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Mathématiques logiques');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Gestion base de données');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Mécanique des fluides');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Mécanique des matériaux');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Electricité');
+INSERT INTO domaine_expertise(nom_expertise) VALUES ('Electronique');
 
 
 
