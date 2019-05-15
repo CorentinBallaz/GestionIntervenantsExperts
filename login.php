082104b8ae87b9a2a5b1b0719c3f297216c16c39
Debzou <david.petit36@gmail.com>
2019-05-15 16:54:39 +0200

    menu bar

--- login.php
+++ login.php
@@ -49,26 +49,7 @@ if ((isset($_POST['login'])) && (isset($_POST['pwd']))) {
     }
 }
 
-  //   $utilisateur = "Professeur";
-  // 	// on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
-
-  //   echo"Bonjour ";
-  //   echo "<br>";
-  //   echo $_SESSION['login'];
-  //   echo "<br>";
-  //
-  //   //header('location: login.php');
-  //   if ($utilisateur == "Etudiant"){
-  //     echo '<a href="etudiant.php">Page etudiant</a>';
-  //   }else if($utilisateur == "Professeur"){
-  //     echo '<a href="professeur.php">Page prof</a>';;
-  //   }else if($utilisateur == "Expert"){
-  //     echo '<a href="expert.php">Page expert</a>';
-  //   }
-  //
-  // }else{
-  //   echo "Non connecté";
-  // }
+  
 
 
 
--- professeur.php
+++ professeur.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
       <html lang="fr">
       <head>
-        <title>New compte</title>
+        <title>Professeur</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
@@ -14,36 +14,36 @@
 
      	width: : 80px;
      }
-    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    /* Remove the navbar's default margin-bottom and rounded borders */
     .navbar {
       margin-bottom: 0;
       border-radius: 0;
     }
-    
+
     /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
     .row.content {height: 750px}
-    
+
     /* Set gray background color and 100% height */
     .sidenav {
       padding-top: 20px;
       background-color: #f1f1f1;
       height: 100%;
     }
-    
+
     /* Set black background color, white text and some padding */
     footer {
       background-color: #555;
       color: white;
       padding: 15px;
     }
-    
+
     /* On small screens, set height to 'auto' for sidenav and grid */
     @media screen and (max-width: 767px) {
       .sidenav {
         height: auto;
         padding: 15px;
       }
-      .row.content {height:auto;} 
+      .row.content {height:auto;}
     }
   </style>
       </head>
@@ -54,41 +54,14 @@ echo "this is the session of ";
 echo  $_SESSION['login'];
  ?>
 <body>
-	
 
-
-<!-- NAVBAR
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">Logo</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-  -->
 
   <div class="btn-group btn-group-justified">
   <a href="#" class="btn btn-primary">Demande Reçu</a>
   <a href="#" class="btn btn-primary">Compte Rendu Reçu</a>
-  
+
 </div>
-<div class="container-fluid text-center">    
+<div class="container-fluid text-center">
   <div class="row content">
     <div class="col-sm-4 sidenav">
     	<!-- pour affichage des élement -->
@@ -117,9 +90,9 @@ echo  $_SESSION['login'];
 
     <div class="form-group">
   	<div class ="col-xs-8">
-    <div class="col-sm-8 text-left"> 
+    <div class="col-sm-8 text-left">
       <h2>Details de la demande </h2>
-   
+
    <!-- <div class="col-sm-2 sidenav">
       <div class="well">
         <p>ADS</p>
@@ -130,8 +103,8 @@ echo  $_SESSION['login'];
     </div>-->
 
   <br>
-  <br>   
-  <br>         
+  <br>
+  <br>
   <table class="table table-striped">
     <thead>
       <tr>
@@ -161,7 +134,7 @@ echo  $_SESSION['login'];
   <br>
   <br>
   <br>
-  
+
   	</div>
   	<div class ="col-xs-4">
   <label for="sel1">Select list:</label>
@@ -178,11 +151,32 @@ echo  $_SESSION['login'];
 </div>
 
 
-  
+
 </div>
 
-<!--<footer class="container-fluid text-center">
-  <p>Footer Text</p>
-</footer>-->
+
+  <form class="form-group" method="post" action="professeur.php">
+    Domaine :
+    <SELECT name="Domaine" size="1">
+    <?php
+    $db = new PDO('mysql:host=localhost;dbname=gestionintervenantsexperts','root','');
+    $qry = 'SELECT nom_cours FROM cours';
+    //echo $qry;
+    //on recupere le type ( etudiant ,expert , prof )
+    $req = $db->query($qry);
+    while($log = $req->fetch()){
+      $Domaine = $log[0];
+      echo $Domaine;
+      echo "<option>".$Domaine;
+    }
+     ?>
+    </SELECT>
+    <br />
+  <input type="submit" name="Domaine1" class="btn btn-dark">
+ </form>
+ <?php
+    isset($_POST['C'])
+ ?>
+
 
 </body>
